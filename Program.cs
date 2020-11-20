7b886199a2053463aa923b1269b94471fda34855
fieryhenry <74794355+fieryhenry@users.noreply.github.com>
2020-11-20 21:39:03 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Battle_Cats_save_editor
         static void Main()
         {
             string path = Path.Combine("C:/Users/henry_5ufuxnx/Downloads/LethaL-EN/My save editor/save");
-            Console.WriteLine("What do you want to do?\n1. Change Cat food\n2. Change XP\n3. All treasures\n4. All cats upgraded +80\n5. Change leadership");
+            Console.WriteLine("\nWhat do you want to do?\n1. Change Cat food\n2. Change XP\n3. All treasures\n4. All cats upgraded 40+80\n5. Change leadership");
             int Choice = Convert.ToInt32(Console.ReadLine());
 
 
@@ -34,6 +34,9 @@ namespace Battle_Cats_save_editor
                     break;
                 case 5:
                     Leadership(path);
+                    break;
+                case 6:
+                    NP(path);
                     break;
                 default:
                     Console.WriteLine("Please input a number that is recognised");
@@ -81,7 +84,7 @@ namespace Battle_Cats_save_editor
                 byte CatFoodByte = Convert.ToByte(CatFoodStrBase10);
                 byte CatFoodByte2 = Convert.ToByte(CatFoodStr2Base10);
 
-                
+
                 stream.Position = 7;
                 stream.WriteByte(CatFoodByte);
                 stream.Position = 8;
@@ -165,7 +168,7 @@ namespace Battle_Cats_save_editor
                 stream.WriteByte(CatFoodByte2);
             }
             string[] test = { };
-            
+
         }
         static void XP(string path)
         {
@@ -489,7 +492,7 @@ namespace Battle_Cats_save_editor
         static void Treasure(string path)
         {
             using var stream = new FileStream("C:/Users/henry_5ufuxnx/Downloads/LethaL-EN/test/test", FileMode.Open, FileAccess.ReadWrite);
-            for (int i = 2986; i <= 3566; i+=4)
+            for (int i = 2986; i <= 3566; i += 4)
             {
                 stream.Position = i;
                 stream.WriteByte(03);
@@ -498,6 +501,7 @@ namespace Battle_Cats_save_editor
             {
                 stream.Position = i;
                 stream.WriteByte(03);
+                Console.WriteLine("All Treasures");
             }
         }
 
@@ -508,13 +512,20 @@ namespace Battle_Cats_save_editor
             {
                 stream.Position = i;
                 stream.WriteByte(Convert.ToByte(80));
+                Console.WriteLine("All cats +80");
+            }
+            for (int i = 9688; i <= 11880; i += 4)
+            {
+                stream.Position = i;
+                stream.WriteByte(Convert.ToByte(28));
+                Console.WriteLine("All cats level 40+80");
             }
         }
 
         static void Leadership(string path)
         {
 
-            
+
 
             Console.WriteLine("How much leadership do you want(max 255)");
             byte leadership = Convert.ToByte(Console.ReadLine());
@@ -527,17 +538,157 @@ namespace Battle_Cats_save_editor
             stream.Read(allData, 0, length);
             for (int i = 0; i <= length; i++)
             {
-                
+
             }
             Console.WriteLine("Scan Complete");
             for (int j = 0; j < length; j++)
             {
                 //Console.WriteLine(j);
-                if (allData[j] == Convert.ToByte(128) && allData[j + 1] == Convert.ToByte(56) && allData[j + 2] == Convert.ToByte(01) && allData[j+3] == Convert.ToByte(00) && allData[j + 4] == Convert.ToByte(00) && allData[j + 5] == leadershipCurrent && allData[j + 11] == Convert.ToByte(72) && allData[j + 12] == Convert.ToByte(57))
+                if (allData[j] == Convert.ToByte(128) && allData[j + 1] == Convert.ToByte(56) && allData[j + 2] == Convert.ToByte(01) && allData[j + 3] == Convert.ToByte(00) && allData[j + 4] == Convert.ToByte(00) && allData[j + 5] == leadershipCurrent && allData[j + 11] == Convert.ToByte(72) && allData[j + 12] == Convert.ToByte(57))
                 {
                     stream.Position = j + 5;
                     stream.WriteByte(leadership);
                     Console.WriteLine("Success");
+                }
+            }
+        }
+
+        static void NP(string path)
+        {
+            Console.WriteLine("How much NP do you want(max 65535)");
+            int NP = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much leadership do you have currently?");
+            byte leadershipCurrent = Convert.ToByte(Console.ReadLine());
+            using var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
+
+            int length = (int)stream.Length;
+            byte[] allData = new byte[length];
+            stream.Read(allData, 0, length);
+            for (int i = 0; i <= length; i++)
+            {
+
+            }
+            Console.WriteLine("Scan Complete");
+            for (int j = 0; j < length; j++)
+            {
+
+                if (allData[j] == Convert.ToByte(128) && allData[j + 1] == Convert.ToByte(56) && allData[j + 2] == Convert.ToByte(01) && allData[j + 3] == Convert.ToByte(00) && allData[j + 4] == Convert.ToByte(00) && allData[j + 5] == leadershipCurrent && allData[j + 11] == Convert.ToByte(72) && allData[j + 12] == Convert.ToByte(57))
+                {
+                    string NPHex = Convert.ToString(NP, 16);
+                    Console.WriteLine("Set NP to " + NP);
+
+                    int StrLength = NPHex.Length;
+
+                    char[] CatArr = { '0', '0', '0', '0' };
+                    char[] CatArr2 = { '0', '0' };
+                    char[] CatArr3 = { '0', '0' };
+
+                    if (StrLength == 3)
+                    {
+                        CatArr = NPHex.ToCharArray(0, StrLength);
+                        CatArr2 = NPHex.ToCharArray(0, StrLength - 1);
+                        CatArr3 = NPHex.ToCharArray(0, StrLength - 1);
+
+                        CatArr2[0] = CatArr[1];
+                        CatArr2[1] = CatArr[2];
+                        CatArr3[0] = '0';
+                        CatArr3[1] = CatArr[0];
+
+                        string CatFoodStr = new string(CatArr2);
+                        string CatFoodStr2 = new string(CatArr3);
+
+                        int CatFoodint = int.Parse(CatFoodStr, System.Globalization.NumberStyles.HexNumber);
+                        int CatFood2int = int.Parse(CatFoodStr2, System.Globalization.NumberStyles.HexNumber);
+
+                        string CatFoodStrBase10 = Convert.ToString(CatFoodint, 10);
+                        string CatFoodStr2Base10 = Convert.ToString(CatFood2int, 10);
+
+                        byte CatFoodByte = Convert.ToByte(CatFoodStrBase10);
+                        byte CatFoodByte2 = Convert.ToByte(CatFoodStr2Base10);
+
+
+                        stream.Position = j - 5;
+                        stream.WriteByte(CatFoodByte);
+                        stream.Position = j - 4;
+                        stream.WriteByte(CatFoodByte2);
+                    }
+                    if (StrLength == 4)
+                    {
+                        CatArr = NPHex.ToCharArray(0, StrLength);
+                        CatArr2 = NPHex.ToCharArray(0, StrLength - 2);
+                        CatArr3 = NPHex.ToCharArray(0, StrLength - 2);
+
+                        CatArr2[0] = CatArr[2];
+                        CatArr2[1] = CatArr[3];
+                        CatArr3[0] = CatArr[0];
+                        CatArr3[1] = CatArr[1];
+
+                        string CatFoodStr = new string(CatArr2);
+                        string CatFoodStr2 = new string(CatArr3);
+
+                        int CatFoodint = int.Parse(CatFoodStr, System.Globalization.NumberStyles.HexNumber);
+                        int CatFood2int = int.Parse(CatFoodStr2, System.Globalization.NumberStyles.HexNumber);
+
+                        string CatFoodStrBase10 = Convert.ToString(CatFoodint, 10);
+                        string CatFoodStr2Base10 = Convert.ToString(CatFood2int, 10);
+
+                        byte CatFoodByte = Convert.ToByte(CatFoodStrBase10);
+                        byte CatFoodByte2 = Convert.ToByte(CatFoodStr2Base10);
+
+                        stream.Position = j - 5;
+                        stream.WriteByte(CatFoodByte);
+                        stream.Position = j - 4;
+                        stream.WriteByte(CatFoodByte2);
+                    }
+                    if (StrLength == 2)
+                    {
+                        CatArr = NPHex.ToCharArray(0, StrLength);
+                        CatArr2 = NPHex.ToCharArray(0, StrLength);
+
+                        CatArr2[0] = CatArr[0];
+                        CatArr2[1] = CatArr[1];
+                        string CatFoodStr = new string(CatArr2);
+                        string CatFoodStr2 = new string(CatArr3);
+
+                        int CatFoodint = int.Parse(CatFoodStr, System.Globalization.NumberStyles.HexNumber);
+                        int CatFood2int = int.Parse(CatFoodStr2, System.Globalization.NumberStyles.HexNumber);
+
+                        string CatFoodStrBase10 = Convert.ToString(CatFoodint, 10);
+                        string CatFoodStr2Base10 = Convert.ToString(CatFood2int, 10);
+
+                        byte CatFoodByte = Convert.ToByte(CatFoodStrBase10);
+                        byte CatFoodByte2 = Convert.ToByte(CatFoodStr2Base10);
+
+                        stream.Position = j - 5;
+                        stream.WriteByte(CatFoodByte);
+                        stream.Position = j - 4;
+                        stream.WriteByte(CatFoodByte2);
+                    }
+                    if (StrLength == 1)
+                    {
+                        CatArr = NPHex.ToCharArray(0, StrLength);
+                        CatArr2 = NPHex.ToCharArray(0, StrLength);
+                        CatArr3 = NPHex.ToCharArray(0, StrLength);
+
+                        CatArr2[0] = CatArr[0];
+
+                        string CatFoodStr = new string(CatArr2);
+                        string CatFoodStr2 = new string(CatArr3);
+
+                        int CatFoodint = int.Parse(CatFoodStr, System.Globalization.NumberStyles.HexNumber);
+                        int CatFood2int = int.Parse(CatFoodStr2, System.Globalization.NumberStyles.HexNumber);
+
+                        string CatFoodStrBase10 = Convert.ToString(CatFoodint, 10);
+                        string CatFoodStr2Base10 = Convert.ToString(CatFood2int, 10);
+
+                        byte CatFoodByte = Convert.ToByte(CatFoodStrBase10);
+                        byte CatFoodByte2 = Convert.ToByte(CatFoodStr2Base10);
+
+                        stream.Position = j - 5;
+                        stream.WriteByte(CatFoodByte);
+                        stream.Position = j - 4;
+                        stream.WriteByte(CatFoodByte2);
+                    }
                 }
             }
         }
