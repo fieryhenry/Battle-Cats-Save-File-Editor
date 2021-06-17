888278a9aa60732799e2e0fb2aee17f3faecbec3
fieryhenry <74794355+fieryhenry@users.noreply.github.com>
2021-06-17 17:35:07 +0100

    Fixed a caching issue with newversion.txt

--- Updater/Updater/Updater.cs
+++ Updater/Updater/Updater.cs
@@ -12,22 +12,33 @@ namespace Updater
 {
     class Updater
     {
-        static void Main(string[] args)
+        static void Main()
         {
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-            string folderName = @"newversion.txt";
             File.Delete(@"Battle Cats save editor.exe");
             string application = @"Battle Cats Save Editor.exe";
 
             WebClient webClient = new WebClient();
-            webClient.DownloadFile("https://raw.githubusercontent.com/fieryhenry/Battle-Cats-Save-File-Editor/main/version.txt", folderName);
 
-            string[] lines = System.IO.File.ReadAllLines(@"newversion.txt");
-           
-            string link = "https://github.com/fieryhenry/Battle-Cats-Save-File-Editor/releases/download/" + lines[0] + "/Battle.Cats.save.editor.exe";
-            Console.WriteLine(link + "\nUpdating program to newest version please wait");
-            WebClient webClient2 = new WebClient();
-            webClient2.DownloadFile(link, application);
+            string lines = "";
+            bool skip = false;
+            try
+            {
+                lines = webClient.DownloadString("https://raw.githubusercontent.com/fieryhenry/Battle-Cats-Save-File-Editor/main/version.txt").Trim('\n');
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("No internet connection to update\n");
+                skip = true;
+            }
+
+            if (!skip)
+            {
+                string link = "https://github.com/fieryhenry/Battle-Cats-Save-File-Editor/releases/download/" + lines + "/Battle.Cats.save.editor.exe";
+                Console.WriteLine(link + "\nUpdating program to newest version please wait");
+                WebClient webClient2 = new WebClient();
+                webClient2.DownloadFile(link, application);
+            }
 
             System.Diagnostics.Process.Start(@"Battle Cats Save Editor.exe");
 
