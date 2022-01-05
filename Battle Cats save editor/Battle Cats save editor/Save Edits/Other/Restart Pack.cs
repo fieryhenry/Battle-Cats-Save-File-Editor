d710793b4936e29a1893a9e59b4801eff9ea5093
fieryhenry <git@henrythomas.me.uk>
2022-01-05 09:20:32 +0000

    Added a feature to get the restart pack

---
+++ Battle Cats save editor/Battle Cats save editor/Save Edits/Other/Restart Pack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Cats_save_editor.SaveEdits
+{
+    public class RestartPack
+    {
+        public static void GetRestartPack(string path)
+        {
+            byte[] conditions = { 0xf8, 0x88, 0x01 };
+            int pos = Editor.Search(path, conditions)[0] + 4;
+            using var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
+            stream.Position = pos;
+            stream.WriteByte(1);
+            Console.WriteLine("Successfully gave the restart pack");
+        }
+    }
+}
