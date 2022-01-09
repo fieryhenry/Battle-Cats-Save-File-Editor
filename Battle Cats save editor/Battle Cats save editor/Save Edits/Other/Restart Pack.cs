c9b6da5a6faf6d76f513188b06baba05e9f7909f
fieryhenry <git@henrythomas.me.uk>
2022-01-09 20:35:09 +0000

    Fixed for more saves

--- Battle Cats save editor/Battle Cats save editor/Save Edits/Other/Restart Pack.cs
+++ Battle Cats save editor/Battle Cats save editor/Save Edits/Other/Restart Pack.cs
@@ -11,8 +11,9 @@ namespace Battle_Cats_save_editor.SaveEdits
     {
         public static void GetRestartPack(string path)
         {
-            byte[] conditions = { 0xf8, 0x88, 0x01 };
-            int pos = Editor.Search(path, conditions)[0] + 4;
+            byte[] conditions = { 0xf8, 0x88, 1, 0, 0, 0x68, 0x3c, 0x01 };
+            byte[] mult = { 0, 0, 0, 1, 1, 0, 0, 0 };
+            int pos = Editor.Search(path, conditions, mult: mult)[0] + 4;
             using var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
             stream.Position = pos;
             stream.WriteByte(1);
