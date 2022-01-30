f5add8dfc76c57e385d7ceaf5677851ecf7cadcc
fieryhenry <git@henrythomas.me.uk>
2022-01-30 20:13:29 +0000

    Removed unused code

--- Battle Cats save editor/Battle Cats save editor/Save Edits/Gamatoto - Ototo/Ototo Cat Cannon.cs
+++ Battle Cats save editor/Battle Cats save editor/Save Edits/Gamatoto - Ototo/Ototo Cat Cannon.cs
@@ -91,56 +91,5 @@ namespace Battle_Cats_save_editor.SaveEdits
             }
             Editor.ColouredText($"&Set cat cannons to:\n&{Editor.CreateOptionsList(cat_cannon_types_str, cat_cannon_levels.ToArray())}");
         }
-        public static void CatCannon_old(string path)
-        {
-            using var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
-
-            int length = (int)stream.Length;
-            byte[] allData = new byte[length];
-            stream.Read(allData, 0, length);
-            int pos = 0;
-
-            // Search for cat cannon position
-            for (int i = 0; i < allData.Length; i++)
-            {
-                if (allData[i] == 0 && allData[i + 1] == 0 && allData[i + 2] == 0 && allData[i + 3] == 8 && allData[i + 4] == 0 && allData[i + 5] == 0 && allData[i + 6] == 0 && allData[i + 7] == 0 && allData[i + 8] == 0 && allData[i + 9] == 0 && allData[i + 10] == 0 && allData[i + 11] == 2 && allData[i + 12] == 0 && allData[i + 13] == 0 && allData[i + 14] == 0 && allData[i + 15] == 3 && allData[i + 16] == 0 && allData[i + 17] == 0 && allData[i + 18] == 0)
-                {
-                    pos = i + 15;
-                    break;
-                }
-            }
-            Editor.ColouredText("What cat cannon type do you want to edit?:\n&1.& Improve Base\n&2.& Develop Slow Beam\n&3.& Develop Iron Wall\n&4.& Develop Thunderbolt\n&5.& Develop Waterblast\n&6.& Develop Holy Blast\n&7.& Develop Breakerblast\n&8.& Develop Curseblast\n&You can edit multiple at once by entering multiple numbers separated by spaces\n");
-            string[] answer = Console.ReadLine().Split(' ');
-            for (int i = 0; i < answer.Length; i++)
-            {
-                int choice = int.Parse(answer[i]);
-                int max = 30;
-
-                // Set max upgrade amount
-                if (choice == 1)
-                {
-                    max = 20;
-                }
-                // Position for the cat cannon is unlocked flag
-                int unlockPos = pos + (16 * (choice - 1));
-                stream.Position = unlockPos;
-                stream.WriteByte(3);
-
-                Console.WriteLine($"What level do you want for {choice}? (max {max})");
-                int level = (int)Editor.Inputed();
-                if (level == 0)
-                {
-                    stream.Position = unlockPos;
-                    stream.WriteByte(0);
-                }
-                else if (level > max) level = max;
-                else if (level < 1) level = 1;
-                level -= 1;
-
-                int levelPos = unlockPos + 4;
-                stream.Position = levelPos;
-                stream.WriteByte((byte)level);
-            }
-        }
     }
 }
