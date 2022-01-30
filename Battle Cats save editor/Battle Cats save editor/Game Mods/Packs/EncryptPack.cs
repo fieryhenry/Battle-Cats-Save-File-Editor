2b207828f28cf81ff3379f5a004d6656e39d6f55
fieryhenry <git@henrythomas.me.uk>
2022-01-30 20:07:03 +0000

    Improved look and code

--- Battle Cats save editor/Battle Cats save editor/Game Mods/Packs/EncryptPack.cs
+++ Battle Cats save editor/Battle Cats save editor/Game Mods/Packs/EncryptPack.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,137 +13,141 @@ namespace Battle_Cats_save_editor.Game_Mods
 {
     public class EncryptPack
     {
-        public static void EncryptData(string key)
+        public static void EncryptData()
         {
-            Console.WriteLine("Enter name of .pack file name to be outputed, e.g DataLocal, ImapServer (don't include .pack) + capatalisation must be correct");
-            string name = Console.ReadLine();
+            Console.WriteLine("Please select a folder of game content");
+            CommonOpenFileDialog dialog = new();
+            dialog.InitialDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\game_files";
+            dialog.IsFolderPicker = true;
+            dialog.Title = "Select a folder of game files";
 
-            FolderBrowserDialog fd = new()
+            if (dialog.ShowDialog() != CommonFileDialogResult.Ok)
             {
-                SelectedPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\game_files",
-                Description = "Select a folder of game files to be encrypted into .pack and .list files",
-                ShowNewFolderButton = false
-            };
-            if (fd.ShowDialog() != DialogResult.OK)
-            {
-                Console.WriteLine("Please select a folder of game content!");
-                Editor.Options();
+                EncryptData();
             }
-            string path = fd.SelectedPath;
-            string[] files = Directory.GetFiles(path);
-            string listFile = files.Length + "\n";
-            Console.WriteLine("Making .list file, this may take a minute depending on how many files are used...");
-            int previous = 0;
-            for (int i = 0; i < files.Length; i++)
+            string path = dialog.FileName;
+            string name = Path.GetFileName(path);
+
+            string final_folder_name = "encrypted_files/";
+            if (!Directory.Exists(final_folder_name))
             {
-                string fileName = Path.GetFileName(files[i]);
-
-                FileInfo fi = new (files[i]);
-                int FileLength = (int)fi.Length;
-
-                listFile += $"{fileName},{previous},{FileLength}\n";
-                previous += FileLength;
+                Directory.CreateDirectory(final_folder_name);
             }
+
+            Console.WriteLine("Creating list file...");
+            byte[] list_bytes = CreateListFile(path);
+            File.WriteAllBytes(final_folder_name + name + ".list", list_bytes);
             Console.WriteLine("Done");
-            byte[] list_bytes = FileHandler.AddExtraBytes("", false, Encoding.ASCII.GetBytes(listFile));
-            if (!Directory.Exists("CompFiles/"))
-            {
-                Directory.CreateDirectory("CompFiles/");
-            }
-            Console.WriteLine("List file successfully created");
-            File.WriteAllBytes(@"CompFiles/" + name + ".list", list_bytes);
 
-            using var stream = new FileStream(@"CompFiles/" + name + ".list", FileMode.Open, FileAccess.ReadWrite);
+            Console.WriteLine("Creating pack file...");
+            byte[] pack_bytes = CreatePackFile(path);
+            File.WriteAllBytes(final_folder_name + name + ".pack", pack_bytes);
+            Console.WriteLine("Done");
 
-            int length = (int)stream.Length;
-            byte[] allData = new byte[length];
-            stream.Read(allData, 0, length);
+            Editor.ColouredText($"&Encrypted files can be found at &{Path.GetFullPath(final_folder_name)}{name}.pack&\n");
+
+        }
+        public static byte[] EncryptListFile(string list_file)
+        {
+            using Aes aes = Aes.Create();
 
             byte[] IV = new byte[16];
-            byte[] Key = Encoding.ASCII.GetBytes(key);
+            byte[] Key = Encoding.ASCII.GetBytes("b484857901742afc");
 
+            aes.Key = Key;
+            aes.IV = IV;
+            aes.Padding = PaddingMode.None;
+            aes.Mode = CipherMode.ECB;
+            return EncryptAES(null, aes, Encoding.ASCII.GetBytes(list_file));
+        }
+        public static byte[] CreateListFile(string dir_name)
+        {
+            string[] files = Directory.GetFiles(dir_name);
 
-            string result = name + ".list";
-
-            using Aes aesAlg = Aes.Create();
-            aesAlg.Key = Key;
-            aesAlg.IV = IV;
-            aesAlg.Padding = PaddingMode.None;
-            aesAlg.Mode = CipherMode.ECB;
-
-            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-            using MemoryStream msEncrypt = new(allData);
-            using CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Read);
-
-            byte[] bytess = new byte[length];
-
-            csEncrypt.Read(bytess, 0, length);
-            stream.Close();
-
-            File.WriteAllBytes(@"CompFiles/" + result, bytess);
-            byte[] PackBytes = new byte[previous];
-            int preIndex = 0;
-            Console.WriteLine("Are you using jp 10.8 and up?(yes/no)");
-            string ver = Console.ReadLine();
-            Console.WriteLine("Making .pack file, this may take a minute depending on how many files are used...");
+            string list_file = files.Length + "\n";
+            int offset = 0;
             for (int i = 0; i < files.Length; i++)
             {
-                byte[] bytef = File.ReadAllBytes(files[i]);
-                if (name.Contains("Server"))
-                {
-                    byte[] IV2 = new byte[16];
-                    byte[] Key2 = Encoding.ASCII.GetBytes("89a0f99078419c28");
+                string file = files[i];
+                FileInfo fi = new(file);
+                int length = (int)fi.Length;
+                string file_name = Path.GetFileName(file);
 
-                    using Aes aesAlg2 = Aes.Create();
-                    aesAlg2.Key = Key2;
-                    aesAlg2.IV = IV2;
-                    aesAlg2.Padding = PaddingMode.None;
-                    aesAlg2.Mode = CipherMode.ECB;
-
-                    bytef = EncryptAES(files[i], aesAlg2);
-
-                    //Console.WriteLine($"Decrypted: {Path.GetFileName(files[i])} {i}/{files.Length}");
-                }
-                else if (name.Contains("Local"))
-                {
-                    if (!name.Contains("ImageData"))
-                    {
-                        if (ver.ToLower() == "yes")
-                        {
-                            Key = StringToByteArray("d754868de89d717fa9e7b06da45ae9e3");
-                            IV = StringToByteArray("40b2131a9f388ad4e5002a98118f6128");
-                        }
-                        else
-                        {
-                            Key = StringToByteArray("0ad39e4aeaf55aa717feb1825edef521");
-                            IV = StringToByteArray("d1d7e708091941d90cdf8aa5f30bb0c2");
-                        }
-
-                        using Aes aesAlg2 = Aes.Create();
-                        aesAlg2.Key = Key;
-                        aesAlg2.IV = IV;
-                        aesAlg2.Padding = PaddingMode.None;
-                        aesAlg2.Mode = CipherMode.CBC;
-
-                        bytef = EncryptAES(files[i], aesAlg2);
-                    }
-
-                    //Console.WriteLine($"Decrypted: {Path.GetFileName(files[i])} {i}/{files.Length}");
-                }
-                bytef.CopyTo(PackBytes, preIndex);
-                preIndex += bytef.Length;
+                list_file += $"{file_name},{offset},{length}\n";
+                offset += length;
             }
-            File.WriteAllBytes(@"CompFiles/" + name + ".pack", PackBytes);
-            Console.WriteLine("Done\nThe .list and .pack file can be found in " + Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/CompFiles/");
+            byte[] list_data_encrypted = EncryptListFile(list_file);
+            return list_data_encrypted;
         }
-        public static byte[] StringToByteArray(string hex)
+        public static Tuple<byte[], byte[], CipherMode> GetKeyAndIV(string file_name, string ver)
         {
-            return Enumerable.Range(0, hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(hex.Substring(x, 2), 16)).ToArray();
+            List<byte> IV = new();
+            List<byte> Key = new();
+            CipherMode mode = CipherMode.CBC;
+            if (file_name.Contains("Server"))
+            {
+                IV = new byte[16].ToList();
+                Key = Encoding.ASCII.GetBytes("89a0f99078419c28").ToList();
+                mode = CipherMode.ECB;
+            }
+            else if (file_name.Contains("Local"))
+            {
+                if (!file_name.Contains("ImageData"))
+                {
+                    if (ver.ToLower() == "yes")
+                    {
+                        Key = Editor.StringToByteArray("d754868de89d717fa9e7b06da45ae9e3").ToList();
+                        IV = Editor.StringToByteArray("40b2131a9f388ad4e5002a98118f6128").ToList();
+                    }
+                    else
+                    {
+                        Key = Editor.StringToByteArray("0ad39e4aeaf55aa717feb1825edef521").ToList();
+                        IV = Editor.StringToByteArray("d1d7e708091941d90cdf8aa5f30bb0c2").ToList();
+                    }
+                    mode = CipherMode.CBC;
+                }
+            }
+            return Tuple.Create(Key.ToArray(), IV.ToArray(), mode);
         }
-        public static byte[] EncryptAES(string path, Aes aes)
+        public static byte[] CreatePackFile(string dir_name)
         {
-            byte[] bytef = File.ReadAllBytes(path);
+            Console.WriteLine("Are you using jp 10.8 and up?(yes/no)");
+            string ver = Console.ReadLine();
+
+            string[] files = Directory.GetFiles(dir_name);
+            List<byte> data = new();
+
+            using Aes aes = Aes.Create();
+            aes.Padding = PaddingMode.None;
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file = files[i];
+                if (file.ToLower().Contains("imagedatalocal"))
+                {
+                    byte[] file_data = File.ReadAllBytes(file);
+                    data.AddRange(file_data.ToList());
+                }
+                else
+                {
+                    Tuple<byte[], byte[], CipherMode> aes_data = GetKeyAndIV(file, ver);
+                    aes.Key = aes_data.Item1;
+                    aes.IV = aes_data.Item2;
+                    aes.Mode = aes_data.Item3;
+
+                    data.AddRange(EncryptAES(file, aes));
+                }
+            }
+            return data.ToArray();
+        }
+        public static byte[] EncryptAES(string path, Aes aes, byte[] data = null)
+        {
+            byte[] bytef;
+            if (data != null) bytef = data;
+            else
+            {
+                bytef = File.ReadAllBytes(path);
+            }
+            bytef = FileHandler.AddExtraBytes(null, false, bytef);
 
             ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
             using MemoryStream ms = new(bytef);
