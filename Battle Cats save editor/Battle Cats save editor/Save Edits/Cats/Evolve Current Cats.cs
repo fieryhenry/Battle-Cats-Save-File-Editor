b8fd2e290d1089a7c9abc86de53f0488845ca707
fieryhenry <git@henrythomas.me.uk>
2022-02-24 10:21:36 +0000

    New feature to evolve current cats

---
+++ Battle Cats save editor/Battle Cats save editor/Save Edits/Cats/Evolve Current Cats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Cats_save_editor.SaveEdits
+{
+    public class EvolveCurrentCats
+    {
+        public static void EvolveCurrent(string path)
+        {
+            int[] curr_cats = CatHandler.GetCurrentCats(path);
+            List<int> forms = EvolveCats.GetEvolveForms(path).ToList();
+            List<int> max_forms = Enumerable.Repeat(0, 9).ToList();
+
+            max_forms.AddRange(Editor.EvolvedFormsGetter().ToList());
+            for (int i = 0; i < curr_cats.Length; i++)
+            {
+                if (curr_cats[i] == 1 && max_forms[i] == 2)
+                {
+                    forms[i] = 2;
+                }
+            }
+            EvolveCats.SetEvolveForms(path, forms.ToArray());
+            Console.WriteLine("Successfully set evolve forms");
+        }
+    }
+}
