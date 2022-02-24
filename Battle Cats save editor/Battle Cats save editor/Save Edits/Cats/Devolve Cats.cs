e4cdf79d4995dea1d5a550759d2903919cb97dd3
fieryhenry <git@henrythomas.me.uk>
2022-02-24 10:21:21 +0000

    Added de-evolve cats back

---
+++ Battle Cats save editor/Battle Cats save editor/Save Edits/Cats/Devolve Cats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Cats_save_editor.SaveEdits
+{
+    public class DevolveCats
+    {
+        public static void DevolveAll(string path)
+        {
+            List<int> forms = new();
+            forms.AddRange(Enumerable.Repeat(0, Editor.GetCatAmount(path)));
+
+            EvolveCats.SetEvolveForms(path, forms.ToArray());
+            Console.WriteLine("Successfully removed evolve forms");
+        }
+        public static void DevolveSpecific(string path)
+        {
+            List<int> forms = EvolveCats.GetEvolveForms(path).ToList();
+
+            Editor.ColouredText($"&What cats do you want to edit?\nenter the cat release order of the cat:&https://battle-cats.fandom.com/wiki/Cat_Release_Order& {Editor.multipleVals}:\n");
+            string[] cats = Console.ReadLine().Split(' ');
+            foreach (string cat in cats)
+            {
+                int cat_id = int.Parse(cat);
+                if (cat_id > forms.Count)
+                {
+                    Console.WriteLine($"Error, cat {cat_id} doesn't exist in your current game version");
+                    continue;
+                }
+                forms[cat_id] = 0;
+            }
+            EvolveCats.SetEvolveForms(path, forms.ToArray());
+            Console.WriteLine("Successfully removed evolve forms");
+        }
+    }
+}
