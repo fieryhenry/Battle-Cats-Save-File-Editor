4f6adad297cefad281a7cc5254c9106fe52c27f4
fieryhenry <git@henrythomas.me.uk>
2022-02-24 10:21:47 +0000

    Fixed

--- Battle Cats save editor/Battle Cats save editor/Save Edits/Cats/Specific Upgrade.cs
+++ Battle Cats save editor/Battle Cats save editor/Save Edits/Cats/Specific Upgrade.cs
@@ -35,13 +35,18 @@ namespace Battle_Cats_save_editor.SaveEdits
 
             int[] baseLevels = new int[idInt.Count];
             int[] plusLevels = new int[idInt.Count];
+            int ignore = 0;
             if (input.ToLower() == "yes")
             {
-                Editor.ColouredText($"&What level do you want: &enter the &base& followed by a &+& then the &plus& level you want, e.g 50+80, 30+0, 10+30\n");
+                Editor.ColouredText("&What level do you want?:enter the &base& followed by a &+& then the &plus& level you want, e.g 50+80, 30+0, 10+30\nEnter the base followed by a plus with nothing else to leave the plus value as it is, e.g 50+, or 20+\nEnter " +
+                    "a plus followed by the plus value to leave the base values as they are e.g +20, +50\n");
                 string level = Console.ReadLine();
 
-                int baselevel = int.Parse(level.Split('+')[0]) - 1;
-                int plusLevel = int.Parse(level.Split('+')[1]);
+                Tuple<int, int, int> data = CatHandler.FindIgnore(level);
+
+                int baselevel = data.Item1;
+                int plusLevel = data.Item2;
+                ignore = data.Item3;
 
                 baseLevels = Enumerable.Repeat(baselevel, baseLevels.Length).ToArray();
                 plusLevels = Enumerable.Repeat(plusLevel, plusLevels.Length).ToArray();
@@ -50,17 +55,19 @@ namespace Battle_Cats_save_editor.SaveEdits
             {
                 for (int i = 0; i < idInt.Count; i++)
                 {
-                    Editor.ColouredText($"&What level do you want to upgrade cat {idInt[i]} to: &enter the &base& followed by a &+& then the &plus& level you want, e.g 50+80, 30+0, 10+30\n");
-                    string level = Console.ReadLine();
+                    Editor.ColouredText($"&What level do you want to upgrade cat {idInt[i]} ?:enter the &base& followed by a &+& then the &plus& level you want, e.g 50+80, 30+0, 10+30\nEnter the base followed by a plus with nothing else to leave the plus value as it is, e.g 50+, or 20+\nEnter " +
+                        "a plus followed by the plus value to leave the base values as they are e.g +20, +50\n"); string level = Console.ReadLine();
+                    Tuple<int ,int ,int> data = CatHandler.FindIgnore(level);
 
-                    int baselevel = int.Parse(level.Split('+')[0]) - 1;
-                    int plusLevel = int.Parse(level.Split('+')[1]);
+                    int baselevel = data.Item1;
+                    int plusLevel = data.Item2;
+                    ignore = data.Item3;
 
                     baseLevels[i] = baselevel;
                     plusLevels[i] = plusLevel;
                 }
             }
-            CatHandler.UpgradeCats(path, idInt.ToArray(), plusLevels, baseLevels);
+            CatHandler.UpgradeCats(path, idInt.ToArray(), plusLevels, baseLevels, ignore);
 
             for (int i = 0; i < idInt.Count; i++)
             {
